41e18dd5b3f6ac4fad644760f26f232a00dbadd1
R-gen <33358136+R-gen@users.noreply.github.com>
2017-12-02 04:12:41 +0100

    Update testCog.py

--- testCog/testCog.py
+++ testCog/testCog.py
@@ -2,18 +2,18 @@ import discord
 from discord.ext import commands
 
 class testCog:
-
+    """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def mycom(self):
+        """This does stuff!"""
 
         #Your code will go here
-	await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+        await self.bot.say("I can do stuff!")
 
 def setup(bot):
     bot.add_cog(testCog(bot))
 
-	
