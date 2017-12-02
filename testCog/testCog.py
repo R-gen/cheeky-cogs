db32a48fa4ab92318f668a8916a925fc1a0f7a0f
R-gen <33358136+R-gen@users.noreply.github.com>
2017-12-02 03:52:13 +0100

    Update testCog.py

--- testCog/testCog.py
+++ testCog/testCog.py
@@ -11,13 +11,13 @@ class testCog:
     async def mycom(self):
 
         #Your code will go here
-		await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+	await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
 	
-		embed=discord.Embed(description=" ")
-		embed.set_author(name= ")
-		embed.set_thumbnail(url="https://i.imgur.com/gRyvWkD.png")
-		embed.add_field(name= , value= , inline=False)
-		await self.bot.say(embed=embed)
+	embed=discord.Embed(description=" ")
+	embed.set_author(name= ")
+	embed.set_thumbnail(url="https://i.imgur.com/gRyvWkD.png")
+	embed.add_field(name= , value= , inline=False)
+	await self.bot.say(embed=embed)
 
 def setup(bot):
     bot.add_cog(testCog(bot))
